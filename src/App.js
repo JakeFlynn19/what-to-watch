cd54aa90232cf9579df2e3ceab4bf9b970ef72c9
Jake Flynn <JakeFlynn@Jakes-MBP-4.attlocal.net>
2021-05-24 14:43:03 -0400

    bottom nav

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "./components/Header/Header.js";
+import SimpleBottomNavigation from "./components/MainNav.js";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
     <div className="app">
       
     </div>
+    <SimpleBottomNavigation />
     </>
   );
 }
