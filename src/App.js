fae24f7a06e0d53385e75f08bd1eedb18202c485
Jake Flynn <JakeFlynn@Jakes-MBP-4.attlocal.net>
2021-05-24 14:47:47 -0400

    browser router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header/Header.js";
-import SimpleBottomNavigation from "./components/MainNav.js";
+import Header from "./components/Header/Header";
+import SimpleBottomNavigation from "./components/MainNav";
+import Movies from "./Pages/Movies/Movies";
+import Series from "./Pages/Series/Series";
+import Trending from "./Pages/Trending/Trending";
+import Search from "./Pages/Search/Search";
+import { Container } from "@material-ui/core";
+
 
 function App() {
   return (
-    <>
-    <Header />
-    <div className="app">
-      
-    </div>
-    <SimpleBottomNavigation />
-    </>
+    <BrowserRouter>
+      <Header />
+      <div className="app">
+        <Container>
+          <Switch>
+            <Route path="/" component={Trending} exact />
+            <Route path="/movies" component={Movies} />
+            <Route path="/series" component={Series} />
+            <Route path="/search" component={Search} />
+          </Switch>
+        </Container>
+      </div>
+      <SimpleBottomNavigation />
+    </BrowserRouter>
   );
 }
 
